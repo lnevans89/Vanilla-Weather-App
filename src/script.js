c330a2fe2bf77ad966ebb9d416a6b9a6893879a3
lnevans89 <lnevans89@gmail.com>
2023-09-22 10:39:33 -0500

    Showing the current temperature

--- src/script.js
+++ src/script.js
@@ -1,1 +1,21 @@
+function displayTemperature (response) {console.log(response.data.main.temp);
+  console.log(response.data);
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+let percipitationElement = document.querySelector("#percipitation");
+temperatureElement.innerHTML = Math.round (response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round (response.data.wind.speed);
+percipitationElement.innerHTML = response.data.main.percipitation;
 
+}
+let apiKey = "729ec012e09d75893dd32df26e7e21a4";
+let city = "Memphis";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
