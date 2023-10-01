8c0386def968d231a9c95fa2dc2a8156746bf055
lnevans89 <lnevans89@gmail.com>
2023-09-30 21:11:39 -0500

    Trying to fix humidity and windspeed issue
    I can't get the humidity and windspeed to show on the weather app.

--- src/script.js
+++ src/script.js
@@ -1,18 +1,22 @@
-function displayTemperature (response) {console.log(response.data.main.temp);
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
   console.log(response.data);
-let temperatureElement = document.querySelector("#temperature");
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let windElement = document.querySelector("#wind");
-let percipitationElement = document.querySelector("#percipitation");
-temperatureElement.innerHTML = Math.round (response.data.main.temp);
-cityElement.innerHTML = response.data.name;
-descriptionElement.innerHTML = response.data.weather[0].description;
-humidityElement.innerHTML = response.data.main.humidity;
-windElement.innerHTML = Math.round (response.data.wind.speed);
-percipitationElement.innerHTML = response.data.main.percipitation;
 
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let precipitationElement = document.querySelector("#precipitation");
+  let dateElement = document.querySelector("#date");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp) + "°F";
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  precipitationElement.innerHTML = response.data.main.percipitation;
+  dateElement.innerHTML = "Friday 7:11";
 }
 let apiKey = "729ec012e09d75893dd32df26e7e21a4";
 let city = "Memphis";
