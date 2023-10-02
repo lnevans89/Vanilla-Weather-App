032319c0697ac8d2b92a0364017aeffb7b2c805d
lnevans89 <lnevans89@gmail.com>
2023-10-01 22:21:28 -0500

    Added new javascript
    Still having trouble getting windspeed, time and precipitation to show on app.   Asked chatgpt to help with javascript code. Even after help from chatgpt time, windspeed and precipitation won't show on app.

--- src/script.js
+++ src/script.js
@@ -1,7 +1,19 @@
-function displayTemperature(response) {
-  console.log(response.data.main.temp);
-  console.log(response.data);
+function formatDate(timestamp) {
+  let date = new Date(timestamp * 1000); // Convert the timestamp to milliseconds
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
 
+  // Ensure that the minutes are displayed with leading zeros if they are less than 10
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -15,11 +27,19 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  precipitationElement.innerHTML = response.data.main.percipitation;
-  dateElement.innerHTML = "Friday 7:11";
+
+  // Note: Typo in "precipitation" fixed, and it's better to check if the field exists in the response
+  if (response.data.hasOwnProperty("rain")) {
+    precipitationElement.innerHTML = response.data.rain["1h"] + "mm";
+  } else {
+    precipitationElement.innerHTML = "N/A";
+  }
+
+  dateElement.innerHTML = formatDate(response.data.dt);
 }
+
 let apiKey = "729ec012e09d75893dd32df26e7e21a4";
 let city = "Memphis";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-console.log(apiUrl);
+
 axios.get(apiUrl).then(displayTemperature);
