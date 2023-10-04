b54c50ab609a5dc3a1ad30a9ce1f9591c1e3e404
lnevans89 <lnevans89@gmail.com>
2023-10-03 21:15:37 -0500

    Added api icon

--- src/script.js
+++ src/script.js
@@ -26,12 +26,16 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let precipitationElement = document.querySelector("#precipitation");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp) + "°F";
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
 
   
   if (response.data.hasOwnProperty("rain")) {
