17d9b21a3b2a5933890ce1ffea6bad34f7d65b7c
lnevans89 <lnevans89@gmail.com>
2023-10-12 14:35:50 -0500

    Updated Api Key

--- src/script.js
+++ src/script.js
@@ -43,9 +43,10 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates)
-  let apiKey = "729ec012e09d75893dd32df26e7e21a4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(coordinates);
+
+  let apiKey = "5863935ee9cca4c02ed68203f807c65b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -85,7 +86,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "729ec012e09d75893dd32df26e7e21a4";
+  let apiKey = "5863935ee9cca4c02ed68203f807c65b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
